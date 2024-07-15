f157ab94ef6d53b1bca2f10dc2c7fad754ee1420
praveensinghkuntia77390 <ps7324900@gmail.com>
2024-07-15 12:58:02 +0530

    initial commit

---
+++ string/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function replaceString(str,target,replacement){
+//     console.log("Original String:",str);
+//     console.log("After replace:",str.replace(target,replacement));
+// }
+// replaceString("hello World","world","JavaScript");
