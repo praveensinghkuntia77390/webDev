85de68a90cf1e01f21288633be870183a4adf095
praveensinghkuntia77390 <ps7324900@gmail.com>
2024-07-16 10:08:23 +0530

    initial commit

---
+++ string/split.js
@@ -0,0 +1,8 @@
+function splitString(str,separator){
+    console.log("Original string:",str);
+    console.log("After split:",str.split(separator));
+}
+splitString("Hello my name is harkirat"," ");
+splitString("Hello my name is harkirat",",");
+splitString("Hello,my,name,is,harkirat"," ");
+splitString("Hello my name is harkirat",",");
---
+++ string/trim.js
@@ -0,0 +1,5 @@
+function trimString(str){
+    console.log("Original String:",str);
+    console.log("After trim:",str.trim());
+}
+trimString("    Hello   this is Praveen Singh   ");
