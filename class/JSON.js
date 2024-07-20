a848c1e3b30642c5282cd6e683e34163e4abf3cf
praveensinghkuntia77390 <ps7324900@gmail.com>
2024-07-20 08:39:28 +0530

    initial commit

--- class/JSON.js
+++ class/JSON.js
@@ -15,3 +15,24 @@ Age:24
 
  const finalString = JSON.stringify(user);
 console.log(finalString);
+
+//Same Program 
+
+function jsonMethods(jsonString) {
+    console.log("Original JSON String:", jsonString);
+  
+    // Parsing JSON string to JavaScript object
+    let parsedObject = JSON.parse(jsonString);
+    console.log("After JSON.parse():", parsedObject);
+  
+    // Stringifying JavaScript object to JSON string
+    let jsonStringified = JSON.stringify(parsedObject);
+    console.log("After JSON.stringify():", jsonStringified);
+  }
+  
+  // Example Usage for JSON Methods
+  const sampleJSONString =
+    '{"key": "value", "number": 42, "nested": {"nestedKey": "nestedValue"}}';
+  
+  jsonMethods(sampleJSONString);
+  
