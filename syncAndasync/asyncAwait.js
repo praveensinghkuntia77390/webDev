c3f5082826dc82bf15a070cf40f507944f49c937
praveensinghkuntia77390 <ps7324900@gmail.com>
2024-07-23 09:19:55 +0530

    initial commit

---
+++ syncAndasync/asyncAwait.js
@@ -0,0 +1,14 @@
+function kiratsAsyncFunction(){
+    let p = new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("hii there")},1000)
+        })
+        return p;
+    }
+
+    async function main(){
+        let value = await kiratsAsyncFunction()
+        console.log(value);
+    }
+
+    main();
