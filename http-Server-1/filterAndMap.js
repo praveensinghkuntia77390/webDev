daaa202a954f0e11b39bb31e760aa5f7a7128059
praveensinghkuntia77390 <ps7324900@gmail.com>
2024-08-06 10:35:01 +0530

    initial commit

---
+++ http-Server-1/filterAndMap.js
@@ -0,0 +1,16 @@
+//filter
+let arr=[2,3,4,5,6,7,8];
+let newarr= arr.filter((val)=>{
+    console.log(val);
+    return val%2==0;
+})
+console.log(newarr);
+
+
+//map
+let ar=[2,3,4,5,6,7];
+let newar= ar.map((value)=>{
+    console.log(value);
+    return value*2;
+})
+console.log(newar);
