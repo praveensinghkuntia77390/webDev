6dce78dc3e03446c2fea56325dc6a1b9cdcb56d4
praveensinghkuntia77390 <ps7324900@gmail.com>
2024-08-21 11:15:44 +0530

    initial.commit

--- http-Server-1/zod.js
+++ http-Server-1/zod.js
@@ -1,27 +1,25 @@
 const express = require("express");
-const zod = require("zod")
+const zod = require("zod");
 const app = express();
 
 const schema = zod.array(zod.number());
 
-app.use(express.json())
+app.use(express.json());
 
  app.post("/health-checkup",function(req,res){
-    //kidney=[1,2]
-    const kidneys = req.body.kidneys;
- const response = schema.safeParse(kidneys)
-
-    res.send({
+ const kidneys = req.body.kidneys;
+ const response = schema.safeParse(kidneys);
+ if(!response.success){
+    res.status(411).json({
+        msg:"The input is inValid",
+    })
+ }else{
+res.send({
         response
     });
+}
  })
 
-//  app.use(function(err,req,res,next){
-//     res.json({
-//         msg:"Oops...! Something went wrong with the Server"
-//     })
-//  })
-
- app.listen(5005,()=>{
-    console.log("Server is running on port 5007");
+ app.listen(5003,()=>{
+    console.log("Server is running on port 5003");
  });
